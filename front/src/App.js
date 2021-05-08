3a3ed49d43d3932826e9bc14218dc0a0d7cc4523
dickycom <dickycom1@gmail.com>
2021-05-08 10:44:21 -0400

    fixed cors backend

--- front/src/App.js
+++ front/src/App.js
@@ -6,73 +6,67 @@ import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import AuthService from './components/auth/auth-service';
 
-import Chat from './components/chat/Chat.js'
+import Chat from './components/chat/Chat.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-
 class App extends Component {
-  state = { loggedInUser: null };
+	state = { loggedInUser: null };
 
-  service = new AuthService();
+	service = new AuthService();
 
-  fetchUser() {
-    if (this.state.loggedInUser === null) {
-      this.service
-        .loggedin()
-        .then((response) => {
-          this.setState({
-            loggedInUser: response,
-          });
-        })
-        .catch((err) => {
-          this.setState({
-            loggedInUser: false,
-          });
-        });
-    }
-  }
+	fetchUser() {
+		if (this.state.loggedInUser === null) {
+			this.service
+				.loggedin()
+				.then((response) => {
+					this.setState({
+						loggedInUser: response,
+					});
+				})
+				.catch((err) => {
+					this.setState({
+						loggedInUser: false,
+					});
+				});
+		}
+	}
 
-  getTheUser = (userObj) => {
-    this.setState({
-      loggedInUser: userObj,
-    });
-  };
+	getTheUser = (userObj) => {
+		this.setState({
+			loggedInUser: userObj,
+		});
+	};
 
-  render() {
-    this.fetchUser();
-    if (this.state.loggedInUser) {
-      return (
-        <div className="App">
-          <Navbar userInSession={this.state.loggedInUser} />
-          <Switch>
-            <Route exact path="/Chat" component={Chat} />
-          </Switch>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <Navbar userInSession={this.state.loggedInUser} />
-          <Router>
-            <Switch>
-              <Route
-                exact
-                path="/signup"
-                render={() => <Signup getUser={this.getTheUser} />}
-              />
-              <Route
-                exact
-                path="/"
-                render={() => <Login getUser={this.getTheUser} />}
-              />
-            </Switch>
-            <Route exact path="/Chat" component={Chat} />
-          </Router>
-          <Route exact path="/Chat" component={Chat} />
-        </div>
-      );
-    }
-  }
+	render() {
+		this.fetchUser();
+		if (this.state.loggedInUser) {
+			return (
+				<div className="App">
+					<Navbar userInSession={this.state.loggedInUser} />
+					<Switch>
+						<Route exact path="/Chat" component={Chat} />
+					</Switch>
+				</div>
+			);
+		} else {
+			return (
+				<div className="App">
+					<Navbar userInSession={this.state.loggedInUser} />
+					<Router>
+						<Switch>
+							<Route
+								exact
+								path="/signup"
+								render={() => <Signup getUser={this.getTheUser} />}
+							/>
+							<Route exact path="/" render={() => <Login getUser={this.getTheUser} />} />
+						</Switch>
+						<Route exact path="/Chat" component={Chat} />
+					</Router>
+				</div>
+			);
+		}
+	}
 }
 
 export default App;
