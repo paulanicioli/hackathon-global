e2f1440e3800d6f7132b145ffabdddafb17fa2e0
Paula Nicioli <paulanicioli@gmail.com>
2021-05-09 11:48:47 -0300

    fixes overall cors config

--- app.js
+++ app.js
@@ -26,7 +26,12 @@ require('./configs/passport.config');
 require('./configs/mongodb.config');
 
 // Middleware Setup
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
